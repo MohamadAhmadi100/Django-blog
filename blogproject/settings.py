5affc377ef4b96c8e3795f38ae69fc7ba3fbd341
Mohamad Ahmadi <mohammadahmadidezhnet@gmail.com>
2021-03-11 01:33:19 +0330

    changed settings.py

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'db=u7cc1$fzfiqqun9l3g!k!bku4+c0fbgdg!6a&yk@bzr6wkk'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -132,5 +132,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'mohammadahmadidezhnet@gmail.com'
-EMAIL_HOST_PASSWORD = 'bardia2589875347'  # 'kczpipjfznapmglo'   os.environ.get('DB_USER')
+EMAIL_HOST_USER = os.environ.get('DB_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('DB_PASS')
